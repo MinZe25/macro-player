2564eb62de0de3076baf6861078fdaf3260acf2a
Aitor Garcia Diez <bbminze@gmail.com>
2023-05-06 15:52:11 +0200

    prints

--- main.py
+++ main.py
@@ -21,23 +21,26 @@ def wait(ms: int):
     time.sleep(ms / 1000)
 
 
+def print_act(act, val):
+    print(f"{act} => {val}")
+
+
 def start():
     with open('./input.csv', 'r') as file:
-        inp = file.read().splitlines()
-    for line in inp:
-        act, val, _ = line.replace('"', '').split(';')
+        inp = [line.replace('"', '').split(';') for line in file.read().splitlines()]
+    for act, val, _ in inp:
         if act == "Key press" or act == "Character(s)":
-            print(line)
+            print_act(act, val)
             key_press(val)
         elif act == "Wait":
-            print(line)
+            print_act(act, val)
             wait(int(val.split(' ')[0]))
         elif act == "Mouse left double click":
-            print(line)
+            print_act(act, val)
             x, y = [int(x) for x in val.replace(' ', '').split(',')]
             click(x, y, True)
         elif act == "Mouse left click":
-            print(line)
+            print_act(act, val)
             x, y = [int(x) for x in val.replace(' ', '').split(',')]
             click(x, y, False)
 
