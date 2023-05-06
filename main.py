5fe1f284ed285d3be3154780efc1e50ff42a26b5
Aitor Garcia Diez <bbminze@gmail.com>
2023-05-06 16:24:31 +0200

    finished program

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import sys
 
 
 def click(x: int, y: int, double=False, left=True):
-    mouse.move(x, y)
+    mouse.move(x, y, True)
     but = mouse.LEFT if left else mouse.RIGHT
     if double:
         mouse.double_click(but)
@@ -25,24 +25,42 @@ def print_act(act, val):
     print(f"{act} => {val}")
 
 
+stop = False
+
+
+def stop_program(a):
+    global stop
+    stop = True
+
+
 def start():
+    global stop
+    keyboard.on_press_key("esc", stop_program)
+    wait(500)
+    times = int(sys.argv[1]) if len(sys.argv) >= 2 else 1
+    print(f"executing {times} times")
     with open('./input.csv', 'r') as file:
         inp = [line.replace('"', '').split(';') for line in file.read().splitlines()]
-    for act, val, _ in inp:
-        if act == "Key press" or act == "Character(s)":
-            print_act(act, val)
-            key_press(val)
-        elif act == "Wait":
-            print_act(act, val)
-            wait(int(val.split(' ')[0]))
-        elif act == "Mouse left double click":
-            print_act(act, val)
-            x, y = [int(x) for x in val.replace(' ', '').split(',')]
-            click(x, y, True)
-        elif act == "Mouse left click":
-            print_act(act, val)
-            x, y = [int(x) for x in val.replace(' ', '').split(',')]
-            click(x, y, False)
+    for i in range(times):
+        print(f"{i} iteration:\n\n")
+        for act, val, _ in inp:
+            if stop:
+                print('Stopping the program')
+                exit(1)
+            elif act == "Key press" or act == "Character(s)":
+                print_act(act, val)
+                key_press(val)
+            elif act == "Wait":
+                print_act(act, val)
+                wait(int(val.split(' ')[0]))
+            elif act == "Mouse left double click":
+                print_act(act, val)
+                x, y = [int(x) for x in val.replace(' ', '').split(',')]
+                click(x, y, True)
+            elif act == "Mouse left click":
+                print_act(act, val)
+                x, y = [int(x) for x in val.replace(' ', '').split(',')]
+                click(x, y, False)
 
 
 if __name__ == "__main__":
