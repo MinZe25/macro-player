a4e0c4c38e677a1a3cb2553cf234180fb40aaa51
Aitor Garcia Diez <bbminze@gmail.com>
2023-05-21 21:32:59 +0200

    improved the script

--- main.py
+++ main.py
@@ -2,10 +2,18 @@ import keyboard
 import mouse
 import time
 import sys
+from screeninfo import get_monitors
+from ctypes import windll
+from os import system
+import ctypes
+
+user32 = windll.user32
+user32.SetProcessDPIAware()
 
 
 def click(x: int, y: int, double=False, left=True):
-    mouse.move(x, y, True)
+    global monitor
+    mouse.move(monitor.x + x, monitor.y + y, True)
     but = mouse.LEFT if left else mouse.RIGHT
     if double:
         mouse.double_click(but)
@@ -35,18 +43,25 @@ def stop_program(a):
 
 def start():
     global stop
+    global monitor
     keyboard.on_press_key("esc", stop_program)
     wait(500)
     times = int(sys.argv[1]) if len(sys.argv) >= 2 else 1
     print(f"executing {times} times")
-    with open('./input.csv', 'r') as file:
+    with open('input_sram_4-3-trool-pavo.csv', 'r') as file:
         inp = [line.replace('"', '').split(';') for line in file.read().splitlines()]
     for i in range(times):
-        print(f"{i} iteration:\n\n")
+        print(f"iteration {i}:\n\n")
+        ctypes.windll.kernel32.SetConsoleTitleW(f"iteration {i}")
         for act, val, _ in inp:
             if stop:
                 print('Stopping the program')
                 exit(1)
+            elif act == "Set Monitor":
+                print_act(act, val)
+                i = int(val)
+                if len(monitors) > i:
+                    monitor = monitors[i]
             elif act == "Key press" or act == "Character(s)":
                 print_act(act, val)
                 key_press(val)
@@ -64,4 +79,9 @@ def start():
 
 
 if __name__ == "__main__":
+    monitors = get_monitors()
+    # uncomment the next lines to know what monitors do you have
+    for m in monitors:
+        print(m)
+    monitor = next(m for m in monitors if m.is_primary)
     start()
